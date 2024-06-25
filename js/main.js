0c9986ef83fccd7169b27f17e44fd50a08157a77
samnejad92 <m.samnejad92@gmail.com>
2024-06-25 22:38:26 +0330

    added to main.js

--- js/main.js
+++ js/main.js
@@ -24,4 +24,75 @@
 		</div>
 	</div>
 */
-
+function renderProducts(products) {
+	const productList = document.getElementById('product-list');
+  
+	const productsHTML = products
+	  .map(
+		product =>
+		  `<div class="product col-lg-4 col-md-6 mb-4">
+		  <div class="card h-100">
+			  <a href="#"
+				  ><img
+					  class="card-img-top"
+					  src="${product.image}"
+					  alt="${product.title}"
+			  /></a>
+			  <div class="card-body">
+				  <h4 class="card-title">
+					  ${product.title}
+				  </h4>
+				  <h5 class="product-price">${formatMoney(product.price)} تومان</h5>
+				  <p class="card-text">
+					  ${product.description}
+				  </p>
+			  </div>
+			  <div class="card-footer">
+				  <button class="btn btn-light add-to-cart" data-product-id="${product.id}">
+					  افزودن به سبد خرید
+				  </button>
+			  </div>
+		  </div>
+	  </div>`
+	  )
+	  .join('');
+  
+	productList.innerHTML = productsHTML;
+  }
+  
+  function formatMoney(amount, decimalCount = 0, decimal = '.', thousands = ',') {
+	try {
+	  decimalCount = Math.abs(decimalCount);
+	  decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+  
+	  const negativeSign = amount < 0 ? '-' : '';
+  
+	  let i = parseInt(
+		(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount))
+	  ).toString();
+	  let j = i.length > 3 ? i.length % 3 : 0;
+  
+	  return (
+		negativeSign +
+		(j ? i.substr(0, j) + thousands : '') +
+		i.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + thousands) +
+		(decimalCount
+		  ? decimal +
+			Math.abs(amount - i)
+			  .toFixed(decimalCount)
+			  .slice(2)
+		  : '')
+	  );
+	} catch (e) {
+	  console.log(e);
+	}
+  }
+  
+  let products = [];
+  window
+	.fetch('http://localhost:3000/products')
+	.then(res => res.json())
+	.then(result => {
+	  products = result;
+	  renderProducts(products);
+	});
